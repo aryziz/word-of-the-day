6d25002ec32b52f75b7f6f8937617530689488b8
Aryan Aziz Mathiesen <aryanam@uia.no>
2024-08-22 21:21:04 +0200

    Formatting

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,16 @@
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/cats',
-                destination: 'https://meowfacts.herokuapp.com',
-            },
-            {
-                source: "/api/:path*",
-                destination: "http://localhost:8080/api/:path*"
-            }
-        ];
-    }
+  async rewrites() {
+    return [
+      {
+        source: "/cats",
+        destination: "https://meowfacts.herokuapp.com",
+      },
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
